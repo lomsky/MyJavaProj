36639e8d10f43bbc9688caa78302869ff8d3fd86
lomsky <dvleonov@mail.ru>
2016-07-11 20:29:35 +0300

    enum

---
+++ src/ru/javawebinar/webapp/model/ContactType.java
@@ -0,0 +1,14 @@
+package ru.javawebinar.webapp.model;
+
+/**
+ * dima
+ * 11.07.16.
+ */
+public enum ContactType {
+    PHONE,
+    MOBILE,
+    HOME_PHONE,
+    SKYPE,
+    MAIL,
+    ICQ
+}
--- src/ru/javawebinar/webapp/model/Resume.java
+++ src/ru/javawebinar/webapp/model/Resume.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class Resume {
     private String fullName;
     private String location;
+    private String homepage;
 
     private List<Contact> contacts;
     private List<Section> sections;
