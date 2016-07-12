7a26a5fe8dcf3b1a1c189d339103ab97d3f624e5
lomsky <dvleonov@mail.ru>
2016-07-12 21:20:59 +0300

    add resume constr-rs

--- src/ru/javawebinar/webapp/model/Resume.java
+++ src/ru/javawebinar/webapp/model/Resume.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.webapp.model;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * dima
@@ -12,12 +14,25 @@ public class Resume {
     private String location;
     private String homepage;
 
-    private List<Contact> contacts;
-    private List<Section> sections;
+    private List<Contact> contacts = new LinkedList<>();
+    private List<Section> sections = new LinkedList<>();
 
     public Resume(String fullName, String location) {
+        this(UUID.randomUUID().toString(), fullName, location);
+    }
+
+    public Resume(String uuid, String fullName, String location) {
+        this.uuid = uuid;
         this.fullName = fullName;
         this.location = location;
+    }
+
+    public void addSection(Section section) {
+        sections.add(section);
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
     }
 
     @Override
@@ -34,5 +49,29 @@ public class Resume {
     @Override
     public int hashCode() {
         return uuid.hashCode();
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public List<Section> getSections() {
+        return sections;
     }
 }
