bdec5b443cb958908e1ffa371d43c6b2e957e39c
lomsky <dvleonov@mail.ru>
2016-07-14 21:51:52 +0300

    array strg

--- src/ru/javawebinar/webapp/storage/ArrayStorage.java
+++ src/ru/javawebinar/webapp/storage/ArrayStorage.java
@@ -1,8 +1,11 @@
 package ru.javawebinar.webapp.storage;
 
+import ru.javawebinar.webapp.WebAppException;
 import ru.javawebinar.webapp.model.Resume;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.logging.Logger;
 
 /**
  * dima
@@ -10,34 +13,50 @@ import java.util.Collection;
  */
 public class ArrayStorage implements IStorage {
     private static final int LIMIT = 100;
+
+    //protected Logger LOGGER = Logger.getLogger(getClass().getName());
+    private static Logger LOGGER = Logger.getLogger(ArrayStorage.class.getName());
     private Resume[] array = new Resume[LIMIT];
+    private int size = 0;
     int idx = 0;
 
     @Override
     public void clear() {
-
+        LOGGER.info("Delete all resumes");
+        Arrays.fill(array, null);
+        size = 0;
     }
 
     @Override
     public void save(Resume r) {
-        for (int i = 0; i < LIMIT; i++) {
-            Resume resume = array[i];
-            if (resume != null) {
-                if (r.equals(resume)) {
-                    throw new IllegalStateException("Уже есть такое резюме");
-                }
 
-            }
-        }
-        for (int i = 0; i < LIMIT; i++) {
-            if (array[i] == null) {
-                array[i] = r;
-            }
-        }
+//        for (int i = 0; i < LIMIT; i++) {
+//            Resume resume = array[i];
+//            if (resume != null) {
+//                if (r.equals(resume)) {
+//                    throw new IllegalStateException("Уже есть такое резюме");
+//                }
+//
+//            }
+//        }
+//        for (int i = 0; i < LIMIT; i++) {
+//            if (array[i] == null) {
+//                array[i] = r;
+//            }
+//        }
+        LOGGER.info("Save resume with uuid " + r.getUuid());
+        int idx = getIndex(r.getUuid());
+        if (idx != -1) throw new WebAppException("Resume " + r.getUuid() + " already exist!", r);
+        array[size++] = r;
     }
 
     @Override
     public void update(Resume r) {
+
+        LOGGER.info("Update resume with uuid " + r.getUuid());
+        int idx = getIndex(r.getUuid());
+        if (idx == -1) throw new WebAppException("Resume " + r.getUuid() + " not exist!", r);
+        array[idx] = r;
 
     }
 
@@ -60,5 +79,15 @@ public class ArrayStorage implements IStorage {
     public Collection<Resume> getAllSorted() {
         return null;
     }
-
+    // на вход уид (из резюме, которое пытаюсь сохранить). если такое резюме есть - возвращается его индекс
+    private int getIndex(String uuid) {
+        for (int i = 0; i < LIMIT; i++) {
+            if (array[i] != null) {
+                if (array[i].getUuid().equals(uuid)) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
 }
